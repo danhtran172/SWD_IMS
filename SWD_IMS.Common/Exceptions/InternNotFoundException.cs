84e437a3e125b427647a62f33a1ac7588bca0f3a
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-06-03 21:54:40 +0700

    Add dependencies

---
+++ SWD_IMS.Common/Exceptions/InternNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD_IMS.Common.Exceptions
+{
+    public sealed class InternNotFoundException : Exception
+    {
+        public InternNotFoundException(string internName) : base($"Intern with name: {internName} cannot be found") { }
+    }
+}
---
+++ SWD_IMS.Entities/DTO/ResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SWD_IMS.Entities.DTO
+{
+    public class ResponseDTO<T>
+    {
+        public int StatusCode { get; set; } = 200;
+        public string? Message { get; set; } = string.Empty;
+        public bool IsSuccess { get; set; } = true;
+        public ResultDTO<T>? Result { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
