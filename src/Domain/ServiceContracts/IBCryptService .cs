75475062b98b567bf40e29e057d219e808bccaac
Trí Sỹ Trương <syttse173472@fpt.edu.vn>
2024-06-21 01:25:17 +0700

    add login and check hash password

--- src/Domain/ServiceContracts/IBCryptService .cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SWD_IMS.src.Domain.ServiceContracts
-{
-    public interface IBCryptService
-    {
-       string HashPassword(string password);
-       bool VerifyPassword(string password, string storedHash);
-    }
-}
